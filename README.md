eab3ecb5aaee196bbbf19b4fcb7ddea4d22136dc
Ben Dill <ben@dillfamily.org>
2017-02-16 23:23:45 -0500

    spec update, so much for tdd

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Create a method that accepts an array of rule identifiers
+  * It should return a rules engine that accepts a number, runs all rules and return the result.
+  * If input in less than or equal to zero, stop processing rules and return value. Otherwise, continue processing.
+  * All rules should be indpendantly testable and reusable.
+
+#### Create the following rules
+* Add five
+* Subtract five
+* Multiply by two
--- src/app.spec.ts
+++ src/app.spec.ts
@@ -18,6 +18,7 @@ describe('app', () => {
 
     describe('rules engine should', () => {
         const ruleSetA = [app.AddFiveRule, app.MultiplyByTwoRule, app.SubtractFiveRule];
+        const ruleSetB = [app.SubtractFiveRule, app.SubtractFiveRule];
 
         it('return the initial value if the initial value is less than or equal to zero', () => {
             var sut = new app.RulesEngine(ruleSetA);
@@ -26,8 +27,20 @@ describe('app', () => {
             expect(sut.Run(-5)).to.eql(-5);
         });
 
-        it('run the rules as expected', () => {
+        it('return expected results', () => {
+            const sut = new app.RulesEngine(ruleSetA);
 
+            expect(sut.Run(1)).to.eql(7);
+            expect(sut.Run(5)).to.eql(15);
+        });
+
+        it('stop processing as expected', () => {
+            const sut = new app.RulesEngine(ruleSetB);
+
+            expect(sut.Run(1)).to.eql(-4);
+            expect(sut.Run(4)).to.eql(-1);
+            expect(sut.Run(6)).to.eql(-4);
+            expect(sut.Run(10)).to.eql(0);
         });
     });
 });
