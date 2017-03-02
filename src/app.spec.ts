c3d370ece4ec23db7ca8825e0f286885f4ab86fc
Ben Dill <ben@dillfamily.org>
2017-03-01 23:35:06 -0500

    updated to allow for rule precondition testing

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -4,26 +4,58 @@ import { expect } from 'chai';
 describe('app', () => {
     describe('rules should', () => {
         it('add five should add five', () => {
-            expect(app.AddFiveRule(5)).to.eql(10);
+            const addFive = new app.AddFiveRule();
+            expect(addFive.Action(5)).to.eql(10);
+            expect(addFive.Action(10)).to.eql(15);
         });
 
         it('subtract five should subtract five', () => {
-            expect(app.SubtractFiveRule(10)).to.eql(5);
+            const subtractFive = new app.SubtractFiveRule();
+            expect(subtractFive.Action(15)).to.eql(10);
+            expect(subtractFive.Action(10)).to.eql(5);
         });
 
         it('multiply by two should, well... multiply by two', () => {
-            expect(app.MultiplyByTwoRule(10)).to.eql(20);
+            const double = new app.MultiplyByTwoRule();
+
+            expect(double.Action(5)).to.eql(10);
+            expect(double.Action(10)).to.eql(20);
+        });
+
+        it('StopIfLTEZero totally does nothing', () => {
+            const stop = new app.StopIfLTEZero();
+
+            expect(stop.Action(-1)).to.eql(-1);
+            expect(stop.Action(10)).to.eql(10);
+        });
+
+        it('call the next rule if present', () => {
+            const root = new app.AddFiveRule();
+            root.Next = new app.MultiplyByTwoRule();
+
+            expect(root.Action(5)).to.eql(20);
+        });
+
+        it('honor preconditions', () => {
+            const root = new app.SubtractFiveRule();
+            root.Next = new app.StopIfLTEZero();
+            root.Next.Next = new app.MultiplyByTwoRule();
+
+            expect(root.Action(4)).to.eql(-1);
+            expect(root.Action(15)).to.eql(20);
         });
     });
 
     describe('rules engine should', () => {
-        const ruleSetA = [app.AddFiveRule, app.MultiplyByTwoRule, app.SubtractFiveRule];
-        const ruleSetB = [app.SubtractFiveRule, app.SubtractFiveRule];
+        const ruleSetA = [new app.AddFiveRule(), new app.MultiplyByTwoRule(), new app.SubtractFiveRule()];
+        const ruleSetB = [new app.SubtractFiveRule(), new app.SubtractFiveRule()];
+        const ruleSetC = [new app.StopIfLTEZero(), new app.AddFiveRule(), new app.MultiplyByTwoRule(), new app.SubtractFiveRule()];
+        const ruleSetD = [new app.SubtractFiveRule(), new app.StopIfLTEZero(), new app.AddFiveRule()];
 
-        it('return the initial value if the initial value is less than or equal to zero', () => {
+        it('add five, double, and subtract five', () => {
             var sut = new app.RulesEngine(ruleSetA);
 
-            expect(sut.Run(0)).to.eql(0);
+            expect(sut.Run(0)).to.eql(5);
             expect(sut.Run(-5)).to.eql(-5);
         });
 
@@ -35,12 +67,16 @@ describe('app', () => {
         });
 
         it('stop processing as expected', () => {
-            const sut = new app.RulesEngine(ruleSetB);
+            let sut = new app.RulesEngine(ruleSetC);
 
-            expect(sut.Run(1)).to.eql(-4);
+            expect(sut.Run(0)).to.eql(0);
+            expect(sut.Run(-1)).to.eql(-1);
+            expect(sut.Run(6)).to.eql(17);
+
+            sut = new app.RulesEngine(ruleSetD);
+
             expect(sut.Run(4)).to.eql(-1);
-            expect(sut.Run(6)).to.eql(-4);
-            expect(sut.Run(10)).to.eql(0);
+            expect(sut.Run(10)).to.eql(10);            
         });
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,88 @@
-interface IRuleIdentifier {
-    (value: number): number;
+interface IRule {
+    PreCondition: (value: number) => boolean;
+    Action: (value: number) => number;
+    Next: IRule;
 }
 
-const AddFiveRule = (value: number) => {
-    return value + 5;
-};
+abstract class BaseRule {
+    public Next: IRule;
 
-const SubtractFiveRule = (value: number) => {
-    return value - 5;
-};
+    PreCondition(value: number): boolean {
+        return true;
+    }
 
-const MultiplyByTwoRule = (value: number) => {
-    return value * 2;
+    Action(value: number): number {
+        if (!this.PreCondition(value)) {
+            return value;
+        }
+
+        const result = this.Calculate(value);
+
+        return (this.Next) ? this.Next.Action(result) : result;
+    }
+
+    abstract Calculate(value: number): number;
+}
+
+class AddFiveRule extends BaseRule {
+    Calculate(value: number): number {
+        return value + 5;
+    }
+}
+
+class SubtractFiveRule extends BaseRule {
+    Calculate(value: number): number {
+        return value - 5;
+    }
+}
+
+class MultiplyByTwoRule extends BaseRule {
+    Calculate(value: number): number {
+        return value * 2;
+    }
+}
+
+class StopIfLTEZero extends BaseRule {
+    PreCondition(value: number): boolean {
+        return value > 0;
+    }
+    Calculate(value: number): number {
+        return value;
+    }
 }
 
 class RulesEngine {
-    constructor(private RuleIdentifiers: IRuleIdentifier[]) {}
+    private rootRule: IRule;
+
+    constructor(private Rules: IRule[]) {
+        if (Rules == null) {
+            throw new Error('gonna need a rule here');
+        }
+
+        // no work in ctor :'(
+        if (Rules.length == 1) {
+            this.rootRule == Rules[0];
+        }
+    }
+
+    private GetRootRule(): IRule {
+        if (this.rootRule) {
+            return this.rootRule;
+        }
+
+        for (let i = this.Rules.length - 1; i >= 1; i--) {
+            this.Rules[i - 1].Next = this.Rules[i];
+        }
+
+        this.rootRule = this.Rules[0];
+
+        return this.rootRule;
+    }
 
     public Run(value: number): number {
-        if (value <= 0) return value;
+        const rule = this.GetRootRule();
 
-        let i = 0;
-        let val = value;
-
-        do {
-            val = this.RuleIdentifiers[i](val);
-            i++;
-        } while (i < this.RuleIdentifiers.length && val > 0)
-
-        return val;
+        return rule.Action(value);
     }
 }
 
@@ -36,5 +90,6 @@ export {
     AddFiveRule,
     SubtractFiveRule,
     MultiplyByTwoRule,
+    StopIfLTEZero,
     RulesEngine
 }
